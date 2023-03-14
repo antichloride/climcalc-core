0d5e4a42573b1165ccea1120688b052a803aebb5
Alexander Rathert (ALRA) <alexander.rathert@ramboll.com>
2023-03-14 23:06:44 +0100

    add api docu

--- src/lib.rs
+++ src/lib.rs
@@ -34,8 +34,21 @@ pub struct Calculator {
 }
 
 
+/// This struct serves as interface. The wasm bridge is not optimized for
+/// objects yet, therefore ids must be used instead.
+///
+/// The procedure works as follows:
+/// 1. Initialize the calculator using the "new" function.
+/// 2. Fill the input parameters using "set_input". This must be done for each
+/// parameter individually.
+/// 3. Add measures using "new_measure".
+/// 4. Trigger the calculation using "calculate_over_years".
+/// 5. Get result values (list with one value per year) using "get_results".
 #[wasm_bindgen]
 impl Calculator{
+    /// A calculator is initialized by defining the year range. Note that
+    /// start and end year are included. All input parameters will be
+    /// initialized as zero.
     pub fn new(start_year: u32, end_year: u32) -> Calculator {
         let calculator = Calculator {
             buildings: Buildings::new(start_year, end_year),
@@ -49,6 +62,7 @@ impl Calculator{
         return calculator;
     }
 
+    /// This function serves to trigger the calculation process.
     pub fn calculate_over_years(&mut self){
         for year in self.start_year..self.end_year + 1{
             self.calculate_first_stage(year);
@@ -103,6 +117,7 @@ impl Calculator{
         return list;
     }
 
+    /// Returns the ids of all input parameters.
     pub fn list_input_ids(&self) -> JsValue{
         let vec: Vec<_> = self.get_inputs().iter().map(|&a| &a.id).collect();
         return to_value(&vec).unwrap();
@@ -122,6 +137,9 @@ impl Calculator{
         }
     }
 
+    /// This function serves to populate the input parameters.
+    ///
+    /// Note that "set input" means the start year value of an input parameter.
     pub fn set_input(&mut self, id: &str, value: f32){
         let res = self.get_input_by_id(id);
         match res{
@@ -133,6 +151,7 @@ impl Calculator{
 
     }
 
+    /// This function returns the start year value of an input parameter.
     pub fn get_input(&mut self, id: &str) -> Option<f32>{
         let res = self.get_input_by_id(id);
         match res{
@@ -153,6 +172,7 @@ impl Calculator{
         return list;
     }
 
+    /// Returns the ids of all results.
     pub fn list_result_ids(&self) -> JsValue{
         let vec: Vec<_> = self.get_results_list().iter().map(|&a| &a.id).collect();
         return to_value(&vec).unwrap();
@@ -173,7 +193,7 @@ impl Calculator{
         }
     }
 
-
+    /// Get a list of values (for each year) in a result.
     pub fn get_results(&mut self, id: &str) -> Vec<f32>{
         let res = self.get_results_by_id(id);
         match res{
@@ -185,6 +205,19 @@ impl Calculator{
 
     }
 
+    /// Add a new measure to a input (given by an input id).
+    /// start and end year defines the year range for which the measure applies.
+    ///
+    /// Note that start year is included.
+    ///
+    /// The delta defines by how much the input parameter is changed over the given range of years.
+    /// The change is applied linear.
+    ///
+    /// I.e. if the number of cars is about to be reduced by 200 from 2025 to
+    /// 2026 and there are 1200 cars in the calculator start year 2023 the
+    /// values for n_cars will be:
+    ///
+    /// 2023->1200, 2024->1200, 2025->1100, 2026->1000, 2027->1000...
     pub fn new_measure(
         &mut self,
         id: &str,
@@ -202,6 +235,8 @@ impl Calculator{
         }
     }
 
+    /// For changing the start_year, end_year or delta of a given measure.
+    /// Note that even if one of the does not change, all must be given.
     pub fn update_measure(
         &mut self,
         id: &str,
@@ -220,6 +255,8 @@ impl Calculator{
 
     }
 
+    /// Deleting a measure. Note that this updates the values of the
+    /// corresponding input.
     pub fn delete_measure(
         &mut self,
         id: &str,
@@ -234,6 +271,7 @@ impl Calculator{
         }
     }
 
+    /// List all measure ids that are present in the current calculator.
     pub fn list_measure_ids(&self) -> JsValue{
         let vec: Vec<_> = self.get_inputs().iter().map(|&a| a.list_measure_ids()).collect();
         return to_value(&vec).unwrap();
